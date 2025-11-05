b8c46e9ab265182bf4a0063b2d50f005ffcf84e8
aryanshah <atshah2@wisc.edu>
2025-11-05 11:51:35 -0600

    Initial publish for Befriend Seniors

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Container, Button } from 'react-bootstrap';
+
+function App() {
+  return (
+    <Container className="text-center mt-5">
+      <h1>Befriend Seniors</h1>
+      <p>
+        Connecting student volunteers with seniors for meaningful virtual conversations.
+      </p>
+      <Button variant="primary">Get Started</Button>
+    </Container>
+  );
+}
+
+export default App;
